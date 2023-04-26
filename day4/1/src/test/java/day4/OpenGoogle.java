4e689df2df7649833655ac823ed18b7855dbd2e3
Akshaya2782003 <119048567+Akshaya2782003@users.noreply.github.com>
2023-04-26 09:30:26 +0530

    Add files via upload

---
+++ day4/1/src/test/java/day4/OpenGoogle.java
@@ -0,0 +1,20 @@
+package day4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(co);
+		driver.get("https://www.google.com");
+
+	}
+
+}
